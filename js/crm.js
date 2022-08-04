f8c9f89054674336d0e187dfdee8a1a95ff9faac
Kartseva Mariya <kartsevaSP@yandex.ru>
2022-08-04 15:11:53 +0300

    init dev

---
+++ js/crm.js
@@ -0,0 +1,15 @@
+const modalTitle = document.querySelector('.modal__title');
+const buttonId = document.querySelector('.vendor-code__btn');
+const goodsId = document.querySelector('.vendor-code__id');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalDiscount = document.querySelector('.modal__input_discount');
+const total = document.querySelector('.modal__total-price');
+
+console.log(modalTitle);
+console.log(buttonId);
+console.log(goodsId);
+console.log(modalForm);
+console.log(modalCheckbox);
+console.log(modalDiscount.value);
+console.log(total);
