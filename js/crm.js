905c7cbdd8afdd37815451b17d96f90c57ec3711
Kartseva Mariya <kartsevaSP@yandex.ru>
2022-08-12 00:44:14 +0300

    add tr in table

--- js/crm.js
+++ js/crm.js
@@ -1,4 +1,65 @@
 'use strict';
+const goods= [
+  {
+    "id": 253842678,
+    "title": "Смартфон Xiaomi 11T 8/128GB",
+    "price": 27000,
+    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
+    "category": "mobile-phone",
+    "discont": false,
+    "count": 3,
+    "units": "шт",
+    "images": {
+      "small": "img/smrtxiaomi11t-m.jpg",
+      "big": "img/smrtxiaomi11t-b.jpg"
+    }
+  },
+  {
+    "id": 296378448,
+    "title": "Радиоуправляемый автомобиль Cheetan",
+    "price": 4000,
+    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+    "category": "toys",
+    "discont": 5,
+    "count": 1,
+    "units": "шт",
+    "images": {
+      "small": "img/cheetancar-m.jpg",
+      "big": "img/cheetancar-b.jpg"
+    }
+  },
+  {
+    "id": 215796548,
+    "title": "ТВ приставка MECOOL KI",
+    "price": 12400,
+    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+    "category": "tv-box",
+    "discont": 15,
+    "count": 4,
+    "units": "шт",
+    "images": {
+      "small": "img/tvboxmecool-m.jpg",
+      "big": "img/tvboxmecool-b.jpg"
+    }
+  },
+  {
+    "id": 246258248,
+    "title": "Витая пара PROConnect 01-0043-3-25",
+    "price": 22,
+    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+    "category": "cables",
+    "discont": false,
+    "count": 420,
+    "units": "v",
+    "images": {
+      "small": "img/lan_proconnect43-3-25.jpg",
+      "big": "img/lan_proconnect43-3-25-b.jpg"
+    }
+  }
+];
+
+let trGoods = [];
+
 const modalTitle = document.querySelector('.modal__title');
 const buttonId = document.querySelector('.vendor-code__btn');
 const goodsId = document.querySelector('.vendor-code__id');
@@ -6,14 +67,47 @@ const modalForm = document.querySelector('.modal__form');
 const modalCheckbox = document.querySelector('.modal__checkbox');
 const modalDiscount = document.querySelector('.modal__input_discount');
 const total = document.querySelector('.modal__total-price');
-
 const modal = document.querySelector('.overlay');
-modal.classList.add('modal__hidden');
+const goodsTable = document.querySelector('.table__body');
 
-console.log(modalTitle);
-console.log(buttonId);
-console.log(goodsId);
-console.log(modalForm);
-console.log(modalCheckbox);
-console.log(modalDiscount.value);
-console.log(total);
+// modal.classList.add('modal__hidden');
+
+modal.classList.toggle('active');
+
+// console.log(modalTitle);
+// console.log(buttonId);
+// console.log(goodsId);
+// console.log(modalForm);
+// console.log(modalCheckbox);
+// console.log(modalDiscount.value);
+// console.log(total);
+
+
+
+const createRow = (good) =>{
+  const trGood = document.createElement('tr');
+  trGood.innerHTML = `
+  <td class="table__cell ">${good.id}</td>
+                <td class="table__cell table__cell_left table__cell_name">${good.title}</td>
+                <td class="table__cell table__cell_left">${good.category}</td>
+                <td class="table__cell">${good.units}</td>
+                <td class="table__cell">${good.count}</td>
+                <td class="table__cell">$${good.price}</td>
+                <td class="table__cell">$${good.price * good.count}</td>
+                <td class="table__cell table__cell_btn-wrapper">
+                  <button class="table__btn table__btn_pic"></button>
+                  <button class="table__btn table__btn_edit"></button>
+                  <button class="table__btn table__btn_del"></button>
+                </td>`;
+  console.log(trGood);
+  return trGood;
+}
+
+const renderGoods = (goods) =>{
+  goodsTable.innerHTML = "";
+  goods.forEach(itemGoods => {
+    goodsTable.append(createRow(itemGoods));
+  })
+}
+
+renderGoods(goods);
